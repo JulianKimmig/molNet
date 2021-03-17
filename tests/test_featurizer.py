85217b38f68f7177c88595f8c8ce2525eab29539
Julian Kimmig <julian.kimmig@gmx.net>
2021-03-17 11:58:53 +0100

    blacked

--- tests/test_featurizer.py
+++ tests/test_featurizer.py
@@ -9,12 +9,12 @@ class MolFeaturizeTest(unittest.TestCase):
     def test_general_atom_feats(self):
         for featurizer in atom_featurizer.__all__:
             mol = rdkit.Chem.MolFromSmiles("CCCCC")
-            f = getattr(atom_featurizer,featurizer)
+            f = getattr(atom_featurizer, featurizer)
             for atom in mol.GetAtoms():
-                    f(atom)
+                f(atom)
 
     def test_general_mol_feats(self):
         for featurizer in molecule_featurizer.__all__:
             mol = rdkit.Chem.MolFromSmiles("c1ccccc1CCl")
-            f = getattr(molecule_featurizer,featurizer)
+            f = getattr(molecule_featurizer, featurizer)
             f(mol)
