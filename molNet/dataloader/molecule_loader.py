3b2050fc3d8d16465e3cf70a288001964922c015
Julian Kimmig <julian.kimmig@gmx.net>
2021-03-17 11:57:46 +0100

    better molecule from df loader

--- molNet/dataloader/molecule_loader.py
+++ molNet/dataloader/molecule_loader.py
@@ -15,8 +15,149 @@ from .base_loader import (
     DirectDataLoader,
     DataFrameGenerator,
     DataFrameDataLoader,
+    PandasDfLoader,
 )
-from ..mol.molecules import Molecule, MolGraph
+from ..mol.molecules import Molecule, molecule_from_smiles, MolGraph
+
+
+class PytorchGeomMolGraphDataLoader(torch_geometric.data.DataLoader):
+    #    def __init__(self, dataset, **kwargs):
+    #       super().__init__(dataset, **kwargs,follow_batch=['graph_features'])
+    pass
+
+
+class MoleculeDfLoader(PandasDfLoader):
+    dataloader = DirectDataLoader
+
+    def __init__(
+        self,
+        df,
+        molecule_column="molNet_mol",
+        mol_create_source="smiles",
+        mol_create_function=molecule_from_smiles,
+        *args,
+        **kwargs
+    ):
+        super().__init__(df, *args, **kwargs)
+        self.molecule_column = molecule_column
+        self.mol_create_source = mol_create_source
+        self.mol_create_function = mol_create_function
+
+    def generate_molecules(self):
+        if self.molecule_column not in self.df.columns:
+            self.df[self.molecule_column] = self.df[self.mol_create_source].apply(
+                self.mol_create_function
+            )
+
+            for c in self.columns:
+                if not c in self.df.columns:
+                    raise Exception("expected collumn '{}' not in dataframe".format(c))
+
+            for r, data in self.df[self.columns].iterrows():
+                molecule = self.df.loc[r, self.molecule_column]
+                for k, d in data.items():
+                    molecule.set_property(k, d)
+
+    def generate_full_dataset(self):
+        for y_column in self.y_columns:
+            if y_column in self.columns:
+                self.columns.remove(y_column)
+        self.generate_molecules()
+        data = []
+        for r, d in self.df[[self.molecule_column] + self.y_columns].iterrows():
+            data.append([d[self.molecule_column], d[self.y_columns].tolist()])
+        return data
+
+
+class MolGraphlDfLoader(MoleculeDfLoader):
+    def __init__(
+        self,
+        df,
+        molgraph_column="molNet_graph",
+        atom_featurizer=None,
+        molecule_featurizer=None,
+        y_atom_featurizer=None,
+        y_molecule_featurizer=None,
+        *args,
+        **kwargs
+    ):
+        super().__init__(df, *args, **kwargs)
+        self.molgraph_column = molgraph_column
+        self.atom_featurizer = atom_featurizer
+        self.molecule_featurizer = molecule_featurizer
+        self.y_atom_featurizer = y_atom_featurizer
+        self.y_molecule_featurizer = y_molecule_featurizer
+
+    def generate_molgraph(self):
+        super().generate_molecules()
+        if self.molgraph_column not in self.df.columns:
+
+            def _moltograph(row):
+                mol = row[self.molecule_column]
+                mg = MolGraph.from_molecule(mol)
+                mg.featurize(
+                    atom_featurizer=self.atom_featurizer,
+                    molecule_featurizer=self.molecule_featurizer,
+                )
+                mg.featurize(
+                    atom_featurizer=self.y_atom_featurizer,
+                    molecule_featurizer=self.y_molecule_featurizer,
+                    as_y=True,
+                )
+                for _y in self.y_columns:
+                    mg.featurize(
+                        molecule_featurizer=lambda mol: row[_y], name=_y, as_y=True
+                    )
+                return mg
+
+            self.df[self.molgraph_column] = self.df[
+                [self.molecule_column] + self.y_columns
+            ].apply(_moltograph, axis=1)
+
+    def generate_full_dataset(self):
+        for y_column in self.y_columns:
+            if y_column in self.columns:
+                self.columns.remove(y_column)
+        self.generate_molgraph()
+        # data = []
+        # for r, d in self.df[[self.molgraph_column] + self.y_columns].iterrows():
+        #    data.append([d[self.molgraph_column], d[self.y_columns].tolist()])
+
+        return self.df[self.molgraph_column].tolist()
+
+
+class PytorchGeomMolDfLoader(MolGraphlDfLoader):
+    dataloader = PytorchGeomMolGraphDataLoader
+
+    def __init__(
+        self,
+        df,
+        pytorchgeomolgraph_column="molNet_ptgeograph",
+        to_graph_input_kwargs={},
+        *args,
+        **kwargs
+    ):
+        super().__init__(df, *args, **kwargs)
+        self.pytorchgeomolgraph_column = pytorchgeomolgraph_column
+        self.to_graph_input_kwargs = to_graph_input_kwargs
+
+    def generate_ptgmmolgraph(self):
+        super().generate_molgraph()
+
+        def _to_mol_graph(molgraph):
+            return molgraph.to_graph_input(**self.to_graph_input_kwargs)
+
+        if self.pytorchgeomolgraph_column not in self.df.columns:
+            self.df[self.pytorchgeomolgraph_column] = self.df[
+                self.molgraph_column
+            ].apply(_to_mol_graph)
+
+    def generate_full_dataset(self):
+        for y_column in self.y_columns:
+            if y_column in self.columns:
+                self.columns.remove(y_column)
+        self.generate_ptgmmolgraph()
+        return self.df[self.pytorchgeomolgraph_column].tolist()
 
 
 class MoleculeFileDataset(FileDataset):
@@ -101,10 +242,6 @@ class PytorchGeomMolLoaderMixin:
 
 
 # loader
-class PytorchGeomMolGraphDataLoader(torch_geometric.data.DataLoader):
-    #    def __init__(self, dataset, **kwargs):
-    #       super().__init__(dataset, **kwargs,follow_batch=['graph_features'])
-    pass
 
 
 # FromCsvLoader
