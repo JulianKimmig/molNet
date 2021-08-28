3aa746ab1f47c9b04a87a87d591ffdf0f7186d8b
Julian Kimmig <julian.kimmig@gmx.net>
2021-08-29 01:20:53 +0200

    Molecule to mol and back

--- molNet/mol/molecule.py
+++ molNet/mol/molecule.py
@@ -1,12 +1,17 @@
-from rdkit.Chem import MolToSmiles, MolFromSmiles, MolFromInchi
+from typing import Union, List, Tuple, Any, Dict
+
 import numpy as np
+import rdkit
+from rdkit.Chem import MolToSmiles, MolFromSmiles, MolFromInchi, rdmolfiles, rdmolops
+from rdkit.Chem.PropertyMol import PropertyMol
+from rdkit.Chem.rdchem import EditableMol
 
 from molNet.utils.identifier2smiles import name_to_smiles
 
-DATATYPES_MAP = {
+DATATYPES_MAP: Dict[str, Tuple[type]] = {
     "FLOAT": (np.floating, float),
     "INT": (np.integer, int),
-    "STRING": (str, np.str),
+    "STRING": (str, np.str_),
     "NONE": (type(None)),
     "BOOL": (bool, np.bool_),
 }
@@ -66,6 +71,8 @@ class MolDataPropertyHolder:
         self._properties[dtype][name] = value
 
     def get_property(self, name, with_dtype=False):
+        if name not in self._names:
+            return None
         index = self._names.index(name)
         dtype = self._dtypes[index]
         if with_dtype:
@@ -75,11 +82,14 @@ class MolDataPropertyHolder:
     def has_property(self, name):
         return name in self._properties
 
-    def get_property_names(self, with_dtype=False):
-        if with_dtype:
-            return list(zip(self._names.copy(), self._dtypes.copy()))
-        else:
-            return self._names.copy()
+    def get_property_names_with_type(self) -> List[Tuple[str, Any]]:
+        return list(zip(self._names.copy(), self._dtypes.copy()))
+
+    def get_property_names(self) -> List[str]:
+        return self._names.copy()
+
+    def get_properties(self) -> Dict[str, Any]:
+        return {p: self.get_property(p) for p in self.get_property_names()}
 
 
 class SMILEError(Exception):
@@ -91,10 +101,25 @@ class MolGenerationError(Exception):
 
 
 class Molecule(MolDataPropertyHolder):
-    def __init__(self, mol, name="", *args, **kwargs):
+    def __init__(self, mol, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self._mol = mol
-        self.name = name
+
+        # restore properties
+        _propnames: List[str] = mol.GetPropNames()
+        for p in _propnames:
+            if p.endswith("__type"):
+                continue
+            v = mol.GetProp(p)
+            t = DATATYPES.STRING
+            if p + "__type" in _propnames:
+                t = mol.GetProp(p + "__type")
+                v = DATATYPES_MAP[t][0](v)
+
+            if p.startswith("molNet_"):
+                p = p.replace("molNet_", "", 1)
+            self.set_property(p, v, t)
+
         self.smiles = MolToSmiles(mol)
 
     @property
@@ -120,7 +145,7 @@ class Molecule(MolDataPropertyHolder):
         return MolToSmiles(self.mol, *args, **kwargs)
 
     @classmethod
-    def from_smiles(cls, smiles, *args, **kwargs):
+    def from_smiles(cls, smiles: str, *args, **kwargs):
         m = MolFromSmiles(smiles)
         if m is None:
             raise MolGenerationError(
@@ -130,7 +155,7 @@ class Molecule(MolDataPropertyHolder):
         return m
 
     @classmethod
-    def from_inchi(cls, inchi, *args, **kwargs):
+    def from_inchi(cls, inchi: str, *args, **kwargs):
         m = MolFromInchi(inchi)
         if m is None:
             raise MolGenerationError(
@@ -140,11 +165,59 @@ class Molecule(MolDataPropertyHolder):
         return m
 
     @classmethod
-    def from_name(cls, name, *args, **kwargs):
+    def from_name(cls, name: str, *args, **kwargs):
         ns = name_to_smiles(name)
         if len(ns) == 0:
             MolGenerationError("no SMILE from name")
-        return cls.from_smiles(smiles=list(ns.keys())[0], name=name, *args, **kwargs)
+
+        molecule = cls.from_smiles(smiles=list(ns.keys())[0], *args, **kwargs)
+        molecule.set_property("name", name, DATATYPES.STRING)
+        return molecule
+
+    def get_mol(
+        self,
+        with_numbers=False,
+        with_H=None,
+        canonical_rank=True,
+        with_properties=True,
+    ) -> rdkit.Chem.Mol:
+        """
+
+        :rtype: rdkit.Chem.Mol
+        """
+        mol = EditableMol(self._mol)
+        mol = mol.GetMol()
+
+        if with_H:
+            mol = rdkit.Chem.AddHs(mol)
+        elif with_H is None:
+            pass
+        else:
+            mol = rdkit.Chem.RemoveHs(mol)
+
+        if canonical_rank:
+            atom_order = rdmolfiles.CanonicalRankAtoms(mol)
+            mol = rdmolops.RenumberAtoms(mol, atom_order)
+
+        if with_numbers:
+            atoms = mol.GetNumAtoms()
+            for idx in range(atoms):
+                mol.GetAtomWithIdx(idx).SetProp(
+                    "molAtomMapNumber", str(mol.GetAtomWithIdx(idx).GetIdx())
+                )
+        if with_properties:
+            mol = PropertyMol(mol)
+            for p, t in self.get_property_names_with_type():
+                mol.SetProp("molNet_" + p, str(self.get_property(p)))
+                # if t != DATATYPES.STRING:
+                mol.SetProp("molNet_" + p + "__type", t)
+        return mol
+
+    def __str__(self) -> str:
+        _name: Union[str, None] = self.get_property("name")
+        if _name is not None and len(_name) > 0:
+            return _name
+        return MolToSmiles(self._mol)
 
 
 def molecule_from_name(name, *args, **kwargs):
--- tests/test_molecule.py
+++ tests/test_molecule.py
@@ -12,8 +12,14 @@ class MolTest(unittest.TestCase):
 
         ns = name_to_smiles(from_name)
         mol = Chem.MolFromSmiles(list(ns.keys())[0])
-        molecule = Molecule(mol, from_name)
+        molecule = Molecule(mol)
+        assert str(molecule) == "Cn1c(=O)c2c(ncn2C)n(C)c1=O"
+        molecule.set_property("name", from_name)
 
         assert molecule.get_smiles() == soll_string
+        assert molecule.get_property("name") == "caffein"
         assert molecule_from_name(from_name).get_smiles() == soll_string
         assert Molecule.from_name(from_name).get_smiles() == soll_string
+        mol = molecule.get_mol()
+        molecule = Molecule(mol)
+        assert str(molecule) == "caffein"
