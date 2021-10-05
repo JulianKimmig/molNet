674a74cbb7a5721dbc31e0902fdfed8904af1090
Julian Kimmig <julian.kimmig@gmx.net>
2021-10-05 17:34:12 +0200

    conformer seed

--- molNet/utils/mol/properties.py
+++ molNet/utils/mol/properties.py
@@ -1,60 +1,80 @@
+import random
+
 from rdkit.Chem import AllChem, AddHs
 from rdkit.Chem.rdchem import Mol
 
 from molNet import ConformerError
 
 
-def create_conformers(mol: Mol, iterations: int = 10) -> Mol:
+def create_conformers(mol: Mol, iterations: int = 100, seed=None) -> Mol:
     if iterations <= 0:
         return mol
+    in_iterations = 1
+    for i in range(iterations):
+        if seed is None:
+            randomSeed = random.randint(1, 10 ** 9)
+        else:
+            randomSeed = seed
+        if mol.GetNumConformers() == 0:
+            mol = AddHs(mol)
+            AllChem.EmbedMolecule(
+                mol,
+                useRandomCoords=False,
+                maxAttempts=in_iterations,
+                randomSeed=randomSeed,
+            )
+        else:
+            return mol
 
-    if mol.GetNumConformers() == 0:
-        mol = AddHs(mol)
-        AllChem.EmbedMolecule(mol, useRandomCoords=False, maxAttempts=iterations)
-    else:
-        return mol
+        if mol.GetNumConformers() == 0:
+            AllChem.EmbedMolecule(
+                mol,
+                useRandomCoords=True,
+                maxAttempts=in_iterations,
+                randomSeed=randomSeed,
+            )
+        else:
+            return mol
 
-    if mol.GetNumConformers() == 0:
-        AllChem.EmbedMolecule(mol, useRandomCoords=True, maxAttempts=iterations)
-    else:
-        return mol
+        if mol.GetNumConformers() == 0:
+            ps = AllChem.ETKDGv2()
+            ps.maxIterations = in_iterations
+            ps.useRandomCoords = True
+            ps.randomSeed = randomSeed
+            AllChem.EmbedMolecule(mol, ps)
+        else:
+            return mol
 
-    if mol.GetNumConformers() == 0:
-        ps = AllChem.ETKDGv2()
-        ps.maxIterations = iterations
-        ps.useRandomCoords = True
-        AllChem.EmbedMolecule(mol, ps)
-    else:
-        return mol
+        if mol.GetNumConformers() == 0:
+            ps = AllChem.ETKDGv3()
+            ps.maxIterations = in_iterations
+            ps.useRandomCoords = True
+            ps.randomSeed = randomSeed
+            AllChem.EmbedMolecule(mol, ps)
+        else:
+            return mol
 
-    if mol.GetNumConformers() == 0:
-        ps = AllChem.ETKDGv3()
-        ps.maxIterations = iterations
-        ps.useRandomCoords = True
-        AllChem.EmbedMolecule(mol, ps)
-    else:
-        return mol
-
-    if mol.GetNumConformers() == 0:
-        AllChem.EmbedMolecule(
-            mol,
-            useRandomCoords=True,
-            maxAttempts=iterations,
-            ignoreSmoothingFailures=True,
-        )
-    else:
-        return mol
+        if mol.GetNumConformers() == 0:
+            AllChem.EmbedMolecule(
+                mol,
+                useRandomCoords=True,
+                maxAttempts=in_iterations,
+                ignoreSmoothingFailures=True,
+                randomSeed=randomSeed,
+            )
+        else:
+            return mol
 
     return mol
 
 
-def create_conformers_if_needed(mol: Mol, iterations: int = 10) -> Mol:
+def create_conformers_if_needed(mol: Mol, iterations: int = 100) -> Mol:
     if mol.GetNumConformers() > 0:
         return mol
     return create_conformers(mol, iterations=iterations)
 
 
-def assert_conformers(mol: Mol, iterations: int = 10) -> Mol:
+def assert_conformers(mol: Mol, iterations: int = 100) -> Mol:
     mol = create_conformers_if_needed(mol, iterations=iterations)
 
     if mol.GetNumConformers() == 0:
