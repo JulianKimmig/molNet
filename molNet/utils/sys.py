026d42dd7aef1b0f897d6810b5e3a3598ad7c504
Julian <julian.kimmig@gmx.net>
2021-11-10 10:22:18 +0100

    editable work dir

--- molNet/utils/sys.py
+++ molNet/utils/sys.py
@@ -3,7 +3,7 @@ if "MOLNET_DIR" not in os.environ:
     os.environ["MOLNET_DIR"]=os.path.join(os.path.expanduser("~"),".molNet")
 
 def set_user_folder(path):
-    os.environ["MOLNET_DIR"]=os.abspath(path)
+    os.environ["MOLNET_DIR"]=os.path.abspath(path)
     
 def get_user_folder():
     return os.environ["MOLNET_DIR"]
