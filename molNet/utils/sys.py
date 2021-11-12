907c8b3d934e70f38088cb47772a635794b61909
Julian Kimmig <julian.kimmig@gmx.net>
2021-11-12 23:17:29 +0000

    env file support

--- molNet/utils/sys.py
+++ molNet/utils/sys.py
@@ -2,8 +2,40 @@ import os
 if "MOLNET_DIR" not in os.environ:
     os.environ["MOLNET_DIR"]=os.path.join(os.path.expanduser("~"),".molNet")
 
-def set_user_folder(path):
-    os.environ["MOLNET_DIR"]=os.path.abspath(path)
+os.makedirs(os.environ["MOLNET_DIR"],exist_ok=True)
+_ENV_FILE=os.path.join(os.environ["MOLNET_DIR"],".env")
+LOKAL_ENVS={}
+
+def _read_env():
+    if not os.path.exists(_ENV_FILE):
+        with open(_ENV_FILE,"w+"):
+            pass
+    with open(_ENV_FILE,"r") as f:
+        cont = f.read()
     
+    for l in cont.split("\n"):
+        try:
+            k,v=l.split("=",1)
+            LOKAL_ENVS[k]=v
+        except Exception:
+            pass
+    for k,v in LOKAL_ENVS.items():
+        os.environ[k]=v
+
+def _write_env():
+    cont="\n".join([f"{k}={v}" for k,v in LOKAL_ENVS.items()])+"\n"
+    with open(_ENV_FILE,"w+") as f:
+        f.write(cont)
+        
+_read_env()
+            
+    
+def set_user_folder(path,permanent=False):
+    os.environ["MOLNET_DIR"]=os.path.abspath(path)
+    os.makedirs(os.environ["MOLNET_DIR"],exist_ok=True)
+    if permanent:
+        LOKAL_ENVS["MOLNET_DIR"]=os.environ["MOLNET_DIR"]
+        _write_env()
+        
 def get_user_folder():
     return os.environ["MOLNET_DIR"]
