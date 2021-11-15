5ca3d8158bc9cb5bf876da039fb0ef5d372e3fc6
Julian Kimmig <julian.kimmig@gmx.net>
2021-11-15 10:47:39 +0000

    logger options

--- molNet/__init__.py
+++ molNet/__init__.py
@@ -1,7 +1,13 @@
 import logging
+import sys
 
+logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+logging.getLogger('matplotlib').setLevel(logging.WARNING)
+
+from rdkit import RDLogger
+lg = RDLogger.logger()
+lg.setLevel(RDLogger.CRITICAL)
 MOLNET_LOGGER = logging.getLogger("molNet")
-
 
 class SMILEError(Exception):
     pass
