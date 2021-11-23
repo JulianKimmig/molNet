fdc56a859f7d90de63684e38174789970542251e
Julian Kimmig <julian.kimmig@gmx.net>
2021-11-23 11:06:55 +0100

    colored logs

--- molNet/__init__.py
+++ molNet/__init__.py
@@ -1,13 +1,17 @@
 import logging
 import sys
 
+import coloredlogs
+
 logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 logging.getLogger('matplotlib').setLevel(logging.WARNING)
 
 from rdkit import RDLogger
+
 lg = RDLogger.logger()
 lg.setLevel(RDLogger.CRITICAL)
 MOLNET_LOGGER = logging.getLogger("molNet")
+coloredlogs.install(level=MOLNET_LOGGER.level, logger=MOLNET_LOGGER)
 
 class SMILEError(Exception):
     pass
