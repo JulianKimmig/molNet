6252ddeffdee551869ece561797c71e37f8b0527
Julian Kimmig <julian.kimmig@gmx.net>
2022-02-22 10:11:48 +0100

    removed old scripts

--- tools/ecdf/ecdf_conf.py
+++
@@ -1,9 +0,0 @@
-from molNet.dataloader.molecular.ChEMBLdb import ChemBLdb29
-
-MOL_DATALOADER=ChemBLdb29
-MOL_DIR=f"/media/julian/ex1ext4_1/molNet/dataloader/{MOL_DATALOADER.__name__}"
-DATADIR=f"/media/julian/ex1ext4_1/molNet/ecdf/{MOL_DATALOADER.__name__}/"
-
-LIMIT_MOLECULES=None
-
-REDRAW=True
