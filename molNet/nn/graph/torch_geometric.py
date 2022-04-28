b00f5a52b13b656e3d4eba5512524d96826aac1f
Julian Kimmig <julian.kimmig@gmx.net>
2022-04-28 14:11:37 +0000

    graph features

--- molNet/nn/graph/torch_geometric.py
+++ molNet/nn/graph/torch_geometric.py
@@ -10,7 +10,7 @@ from numpy import ndarray
 
 from molNet.mol.molgraph import MolGraph
 
-def graph_input_from_edgelist(edgelist,node_features,y=None):
+def graph_input_from_edgelist(edgelist,node_features,y=None,graph_features=None):
     #assert both connection directions
     edgelist=np.concatenate((edgelist,edgelist[[1,0],:]),axis=1)
     edgelist=np.unique(edgelist,axis=1)
@@ -21,6 +21,7 @@ def graph_input_from_edgelist(edgelist,node_features,y=None):
         x=torch.from_numpy(node_features.astype(float)).float(),
         num_nodes=len(node_features),
        # edge_index=torch.from_numpy(edge_index).long(),
+        graph_features=None if graph_features is None else torch.from_numpy(graph_features.astype(float)).float()
     )
 
 def molgraph_arrays_to_graph_input(
